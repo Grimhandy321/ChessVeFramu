a5e6da4a854f4138b5db7490b1ee8aa1b7e14109
Pole200 <michal.jezek07@gmail.com>
2024-05-30 18:22:40 +0200

    i will die now it works

--- src/main/java/com/example/Game/Engine.java
+++ src/main/java/com/example/Game/Engine.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Engine {
-    int searchDepth = 4;
+    int searchDepth = 8;
     public int EvaluatePosition(Board board) {
         int eval = 0;
         eval += board.getPieceLocation(Piece.BLACK_ROOK).size() * 500;
@@ -39,17 +39,17 @@ public class Engine {
             }
         }
 
-        allMoves = allMoves.stream().sorted(Comparator.comparingInt(move -> 8 - getPieceAtSquare(board, move.getTo()).getPieceType().ordinal())).toList();
+        allMoves = allMoves.stream().sorted(Comparator.comparingInt(move -> 8 - getValueOfAtSquare(board, move.getTo()))).toList();
 
         int bestEval = board.getSideToMove() == Side.WHITE ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         ArrayList<Move> bestMoves = new ArrayList<Move>();
 
-        for (int i = 0; i < allMoves.size(); i++) {
+        for (Move allMove : allMoves) {
             int eval = 0;
-            Move current = allMoves.get(i);
+            Move current = allMove;
             board.doMove(current);
             eval += AlphaBeta(board, this.searchDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, board.getSideToMove() == Side.WHITE);
-            board.doMove(current);
+            board.undoMove();
             if (bestMoves.isEmpty()) {
                 bestMoves.add(current);
                 bestEval = eval;
@@ -69,7 +69,7 @@ public class Engine {
 
     public int AlphaBeta(Board node, int depth, int alpha, int beta, boolean maximizingPlayer) {
         List<Move> childNodes = node.legalMoves();
-        childNodes = childNodes.stream().sorted(Comparator.comparingInt(move -> 8 - getPieceAtSquare(node, move.getTo()).getPieceType().ordinal())).toList();
+        childNodes = childNodes.stream().sorted(Comparator.comparingInt(move -> 8 - getValueOfAtSquare(node, move.getTo()))).toList();
         if (depth == 0 || childNodes.isEmpty()) {
             return EvaluatePosition(node);
         }
@@ -100,8 +100,22 @@ public class Engine {
         }
     }
 
-    public Piece getPieceAtSquare(Board board, Square square) {
-        return board.getPiece(square);
+    public int getValueOfAtSquare(Board board, Square square) {
+        Piece piece = board.getPiece(square);
+        int i = 0;
+        switch (piece.getPieceType()){
+            case KNIGHT -> i = 2;
+            case PAWN -> i =1;
+            case ROOK -> i=5;
+            case BISHOP -> i=2;
+            case QUEEN -> i=7;
+            case KING -> i=8;
+            case null -> i=0;
+            case NONE -> {
+                i=0;
+            }
+        }
+        return i;
     }
 
     private boolean MoveIsCheckmate(Board board, Move move) {
